3e3f7d0248c5d1d50765f6b493fc115787aa51f0
Kristián Mičko <kristian.micko@student.tuke.sk>
2020-05-13 23:07:06 +0200

    backend

--- server.py
+++ server.py
@@ -15,3 +15,15 @@ def computing():
     with open('data.JSON') as json_file:
         data = json.load(json_file) 
     return  jsonify(data),200
+
+@app.route('/image', methods=['POST'])
+def compute():
+    image = request.get_json()
+    dict_image = dict(image)
+    myDb = MYSQL.connect(host="147.232.40.14", user="km863qc", passwd="km863qc", database="km863qc", port=3306)
+    cursor = myDb.cursor()
+    cursor.execute("INSERT INTO Nakup (Nakup) VALUES ('{}')".format(data_dict['image']))
+    myDb.commit()
+    cursor.close()
+    myDb.close()
+    return jsonify("Created"),201
